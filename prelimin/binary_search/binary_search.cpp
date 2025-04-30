106172904977b2d0eead5215ae8f1a6ef018eed2
MatheusOliveir4 <matheusfloliveira@gmail.com>
2025-04-30 10:22:53 -0300

    binary_search

---
+++ prelimin/binary_search/binary_search.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+// O(lg n) -> We are considering that the array it's already sorted
+int binary_search(int* A, int size, int k) {
+    int low, high, mid;
+
+    low = 0;
+    high = size - 1;
+
+    while (low <= high)
+    {
+        mid = (low + high) / 2;
+        if (A[mid] == k) {
+            return mid;
+        } else if (A[mid] < k) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+    
+    return -1;
+}
+
+int main() {
+    int size, k, i, res;
+    cin >> size;
+
+    int* A = new int[size];
+
+    for (i = 0; i < size; i++)
+    {
+        cin >> A[i];
+    }
+    
+    cin >> k;
+
+    res = binary_search(A, size, k);
+    cout << res << endl;
+
+    return 0;
+}
